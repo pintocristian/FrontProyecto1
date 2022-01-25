dc0eae12e396142a791c64695dae2f03632d2792
pintocristian <70982875+pintocristian@users.noreply.github.com>
2022-01-24 20:28:30 -0500

    Componente inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  styleUrls: ['./inicio.component.scss'],
+  providers: [AuthService]
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent {
 
-  constructor() { }
-
+  constructor(private authSvc:AuthService) { }
+  
+  public user$: Observable<any> = this.authSvc.afAuth.user;
   ngOnInit(): void {
   }
 
