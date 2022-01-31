d505a6bfcb58a55d79b45752b972cf77f751af7d
pintocristian <70982875+pintocristian@users.noreply.github.com>
2022-01-30 19:55:12 -0500

    update main

--- src/app/labs/leyhooke/practicaleyhooke/practicaleyhooke.module.ts
+++ src/app/labs/leyhooke/practicaleyhooke/practicaleyhooke.module.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FullCalendarModule } from '@fullcalendar/angular';
 
 import { PracticaleyhookeRoutingModule } from './practicaleyhooke-routing.module';
 import { PracticaleyhookeComponent } from './practicaleyhooke.component';
@@ -11,7 +10,6 @@ import { PracticaleyhookeComponent } from './practicaleyhooke.component';
   imports: [
     CommonModule,
     PracticaleyhookeRoutingModule,
-    FullCalendarModule
   ],schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
 })
 export class PracticaleyhookeModule { }
